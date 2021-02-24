ab3fd042bc2c85e36ef05a471703b42da4759bd5
Konrad Droeske <kdroeske@gmail.com>
2021-02-24 10:34:43 -0500

    switched to ts-loader

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       {
         test: /\.tsx?$/,
-        use: 'awesome-typescript-loader',
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
@@ -41,7 +41,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['*', '.ts', '.tsx', '.js', '.jsx', '.json', '.css'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.css'],
   },
   plugins: [
     new HtmlWebpackPlugin({
