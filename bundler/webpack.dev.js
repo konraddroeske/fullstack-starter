6db1997ec8e559add3dcaf0a576c9787b06829a3
Konrad Droeske <kdroeske@gmail.com>
2021-02-24 17:01:09 -0500

    readded postcss loader

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -1,21 +1,22 @@
 const { merge } = require('webpack-merge');
 const commonConfiguration = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(commonConfiguration, {
   mode: 'development',
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/,
-  //       exclude: /node_modules/,
-  //       use: [
-  //         MiniCssExtractPlugin.loader,
-  //         { loader: 'css-loader', options: { sourceMap: true } },
-  //         'postcss-loader',
-  //       ],
-  //     },
-  //   ],
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { sourceMap: true } },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
   devServer: {
     port: 3000,
     open: true,
